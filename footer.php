d718b20033c252ce8bbb93db934db0fa4a10c3e2
SK Kamal <skkamal6301@gmail.com>
2024-01-18 17:28:13 +0600

    navbar added

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Footer file
+*
+* @package MyTheme
+*/
+?>
+
+<footer>Footer</footer>
+</div>
+</div> 
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>HEllo</h1>
-</body>
-</html>
+<?php
+/*
+* Main Template File.
+*
+* @package MyTheme
+*/
+get_header();
+?>
+
+    <div class="content">
+        content
+    </div>
+
+<?php 
+
+get_footer();
